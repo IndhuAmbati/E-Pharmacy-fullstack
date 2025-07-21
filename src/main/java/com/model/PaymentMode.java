c77466e03fe80ec1aace92b413401655f6452126
IndhuAmbati <23wh1a6658@bvrithyderabad.edu.in>
2025-07-21 19:50:11 +0530

    Delete src directory

--- src/main/java/com/model/PaymentMode.java
+++
@@ -1,7 +0,0 @@
-package com.model;
-
-public enum PaymentMode {
-    CARD,
-    UPI,
-    CASH_ON_DELIVERY
-}
--- src/main/java/com/model/PaymentStatus.java
+++
@@ -1,8 +0,0 @@
-package com.model;
-
-public enum PaymentStatus {
-	    PENDING,
-	    SUCCESS,
-	    FAILED
-	
-}
